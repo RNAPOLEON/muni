f00909d67a90649a3e3ca7c1d484430cc60b82db
RNAPOLEÓN <romanonapoloensr@gmail.com>
2024-09-06 10:18:45 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./component/menu/menu.component";
+import { MainComponent } from "./component/main/main.component";
+import { FixedComponent } from "./component/fixed/fixed.component"; 
+import { GridOneComponent } from "./component/grid-one/grid-one.component";
+import { ContenidoComponent } from "./component/contenido/contenido.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, MainComponent, FixedComponent, GridOneComponent, ContenidoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'muni';
---
+++ src/app/component/contenido/contenido.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contenido',
+  standalone: true,
+  imports: [],
+  templateUrl: './contenido.component.html',
+  styleUrls: ['./contenido.component.scss']
+})
+export class ContenidoComponent {
+
+}
